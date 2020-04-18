1ff8897cb5d102d03ee019ae5370123388be4f4f
Philipp <philipp.hoff@outlook.de>
2020-04-18 14:52:23 +0200

    better help menu

--- DiscordBot/app.js
+++ DiscordBot/app.js
@@ -6,64 +6,104 @@ const client = new Discord.Client();
 const music = require('./musicModule.js');
 const util = require('./Utility.js');
 
-function help(message) {
-    //list all commands
+function help(message, args) {
+    //no arguments: print all
+    if (args.length == 0) {
+        args.push("all");
+    }
+    //array of arrays of commands
     commands = [];
-    commands.push({ name: "play <YT-URL>", description: "Add song to the queue", aliases: "add" });
-    commands.push({ name: "search <query, max = 5>", description: "Search for a query on YouTube", aliases: "" });
-    commands.push({ name: "playlist <YT-URL>", description: "Add a YT-playlist to the queue", aliases: "" });
-    commands.push({ name: "pause", description: "Pause the current song", aliases: "p" });
-    commands.push({ name: "resume", description: "Resume after paused", aliases: "res, continue" });
-    commands.push({ name: "skip <int = 1>", description: "Skip multiple songs", aliases: "s" });
-    commands.push({ name: "unskip <int = 1>", description: "Unskip multiple songs", aliases: "us" });
-    commands.push({ name: "queue <page = 1 | all>", description: "Show the queue", aliases: "q" });
-    commands.push({ name: "volume <float = 5.0>", description: "Set the volume", aliases: "vol, v" });
-    commands.push({ name: "shuffle", description: "Shuffle the queue", aliases: "random, randomize" });
-    commands.push({ name: "looping [<true/false>]", description: "Set looping status, if no parameter shows status", aliases: "loop" });
-    commands.push({ name: "count", description: "Show size of the queue", aliases: "c, number" });
-    commands.push({ name: "now", description: "Show what is currently being played", aliases: "np" });
-    commands.push({ name: "again", description: "Play current song again", aliases: "replay, rp" });
-    commands.push({ name: "playnow <YT-URL>", description: "Immediatly play a song", aliases: "playdirect, force" });
-    commands.push({ name: "who", description: "Show who requested this song", aliases: "requested" });
-    commands.push({ name: "remove <int = 1>", description: "Remove song Nr.", aliases: "delete, del" });
-    commands.push({ name: "duplicates", description: "Remove all duplicates from queue", aliases: "removeDoubles" });
-    commands.push({ name: "clear", description: "Clear the entire queue - FINAL!", aliases: "cls" });
-    commands.push({ name: "save <name>", description: "Attach the queue to a file", aliases: "write" });
-    commands.push({ name: "load <name>", description: "Attach songs from a file to the queue", aliases: "l" });
-    commands.push({ name: "help", description: "This menu", aliases: "h" });
+    //name array
+    names = [];
 
-    //find longest name, description
-    longNameInd = 0;
-    longDescInd = 0;
-    for (i = 0; i < commands.length; i++) {
-        if (commands[i].name.length > commands[longNameInd].name.length) {
-            longNameInd = i;
-        }
-        if (commands[i].description.length > commands[longDescInd].description.length) {
-            longDescInd = i;
+    //if listing all, do a certain order:
+    if (args.find(function (elem) { return (elem == "all"); })) {
+        args = ["main", "music"];
+    }
+
+    //remove duplicates to avoid spam
+    for (i = 0; i < args.length; i++) {
+        for (j = i + 1; j < args.length; j++) {
+            if (args[j] == args[i]) {
+                args.splice(j--, 1);
+            }
         }
     }
-    const nameSize = commands[longNameInd].name.length;
-    const descSize = commands[longDescInd].description.length;
-
-    //create command list
-    commandList = "```diff\n";
-    const cur = `+ Currently, there are ${commands.length} commands available +\n`;
-    commandList += cur;
-    commandList += util.trimString("Command <arguments>", nameSize + 3, " ");
-    commandList += util.trimString("Description", descSize + 3, " ");
-    commandList += "Aliases\n";
-    for (i = 0; i < commands.length; i++) {
-        commandList += util.trimString(commands[i].name, nameSize, " ");
-        commandList += " - ";
-        commandList += util.trimString(commands[i].description, descSize, " ");
-        commandList += " - ";
-        commandList += commands[i].aliases;
-        commandList += "\n";
-    }
-    commandList += "```";
     
-    return message.channel.send(commandList);
+    for (i = 0; i < args.length; i++) {
+        moduleCommands = [];
+        switch (args[i]) {
+            case "music":
+                //add name to names array
+                names.push("MUSIC MODULE");
+                //list all music commands
+                moduleCommands.push({ name: "play <YT-URL>", description: "Add song to the queue", aliases: "add" });
+                moduleCommands.push({ name: "search <query, max = 5>", description: "Search for a query on YouTube", aliases: "" });
+                moduleCommands.push({ name: "playlist <YT-URL>", description: "Add a YT-playlist to the queue", aliases: "" });
+                moduleCommands.push({ name: "pause", description: "Pause the current song", aliases: "p" });
+                moduleCommands.push({ name: "resume", description: "Resume after paused", aliases: "res, continue" });
+                moduleCommands.push({ name: "skip <int = 1>", description: "Skip multiple songs", aliases: "s" });
+                moduleCommands.push({ name: "unskip <int = 1>", description: "Unskip multiple songs", aliases: "us" });
+                moduleCommands.push({ name: "queue <page = 1 | all>", description: "Show the queue", aliases: "q" });
+                moduleCommands.push({ name: "volume <float = 5.0>", description: "Set the volume", aliases: "vol, v" });
+                moduleCommands.push({ name: "shuffle", description: "Shuffle the queue", aliases: "random, randomize" });
+                moduleCommands.push({ name: "looping [<true/false>]", description: "Set looping status, if no parameter shows status", aliases: "loop" });
+                moduleCommands.push({ name: "count", description: "Show size of the queue", aliases: "c, number" });
+                moduleCommands.push({ name: "now", description: "Show what is currently being played", aliases: "np" });
+                moduleCommands.push({ name: "again", description: "Play current song again", aliases: "replay, rp" });
+                moduleCommands.push({ name: "playnow <YT-URL>", description: "Immediatly play a song", aliases: "playdirect, force" });
+                moduleCommands.push({ name: "who", description: "Show who requested this song", aliases: "requested" });
+                moduleCommands.push({ name: "remove <int = 1>", description: "Remove song Nr.", aliases: "delete, del" });
+                moduleCommands.push({ name: "duplicates", description: "Remove all duplicates from queue", aliases: "removeDoubles" });
+                moduleCommands.push({ name: "clear", description: "Clear the entire queue - FINAL!", aliases: "cls" });
+                moduleCommands.push({ name: "save <name>", description: "Attach the queue to a file", aliases: "write" });
+                moduleCommands.push({ name: "load <name>", description: "Attach songs from a file to the queue", aliases: "l" });
+                break;
+            case "main":
+                //add name to names array
+                names.push("MAIN MODULE");
+                //list all main commands
+                moduleCommands.push({ name: "help <Module = all>", description: "This menu", aliases: "h" });
+                break;
+            default:
+                util.logUserError("User entered non-registered module", "help", "Parameter: " + args[i]);
+                return message.channel.send("Unknown module: " + args[i]);
+        }
+        commands.push(moduleCommands);
+    }
+
+    //find longest name, description over all shown commands
+    nameSize = 0;
+    descSize = 0;
+    for (p = 0; p < commands.length; p++) {
+        for (i = 0; i < commands[p].length; i++) {
+            nameSize = Math.max(commands[p][i].name.length, nameSize)
+            descSize = Math.max(commands[p][i].description.length, descSize);
+        }
+    }
+
+    //for every command array in commands, make a seperate paragraph
+    for (p = 0; p < commands.length; p++) {
+        //create command list
+        commandList = "```diff\n";
+        const name = "+ " + names[p] + " +\n";
+        commandList += name;
+        const cur = `+ Currently, there are ${commands[p].length} commands available +\n`;
+        commandList += cur;
+        commandList += util.trimString("Command <arguments>", nameSize + 3, " ");
+        commandList += util.trimString("Description", descSize + 3, " ");
+        commandList += "Aliases\n";
+        for (i = 0; i < commands[p].length; i++) {
+            commandList += util.trimString(commands[p][i].name, nameSize, " ");
+            commandList += " - ";
+            commandList += util.trimString(commands[p][i].description, descSize, " ");
+            commandList += " - ";
+            commandList += commands[p][i].aliases;
+            commandList += "\n";
+        }
+        commandList += "```";
+        message.channel.send(commandList);
+    }
 }
 
 client.once("ready", () => {
@@ -88,7 +128,8 @@ client.on("message", async message => {
     if (!message.content.startsWith(prefix)) return;
 
     message.content = message.content.substr(1);
-    const args = message.content.split(" ");
+    var args = message.content.split(" ");
+    args = args.filter(function (value, index, arr) { return (value != ""); });
     const first = args.shift();
 
     switch (first) {
@@ -172,7 +213,7 @@ client.on("message", async message => {
             break;
         case "help":
         case "h":
-            help(message);
+            help(message, args);
             break;
         case "playnow":
         case "playdirect":
